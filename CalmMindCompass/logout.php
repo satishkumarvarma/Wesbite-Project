aad5f9f228b94391419e309905b8e9e3e92b16eb
vikramm0907 <135581252+vikramm0907@users.noreply.github.com>
2023-08-11 15:37:12 +0530

    Delete CalmMindCompass directory

--- CalmMindCompass/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-@include 'config.php';
-// echo '<script>alert("Successfully loged out ..")</script>'; not working.
-session_start();
-session_unset();
-session_destroy();
-
-header('location:index.php');
-?>
